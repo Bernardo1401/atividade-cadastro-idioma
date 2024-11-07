2f6f036de7fef2d60d40b0de2afe0640b3ac0462
Bernardo Marques <sardinhaber@gmail.com>
2024-11-07 14:59:05 -0300

    feat: create class

---
+++ src/routes/models/Idiomas.js
@@ -0,0 +1,9 @@
+class Idioma {
+    constructor(id, idioma, progresso, nivel, dificuldade) {
+        this.id = id;
+        this.idioma = idioma;
+        this.progresso = progresso;
+        this.nivel = nivel;
+        this.dificuldade = dificuldade;
+    }
+}
